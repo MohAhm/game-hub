8fd53817303e47e14b9fd519c4ac3d0c253aa0df
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-02-21 22:20:15 +0100

    Refactor - simplify time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import ApiClient from "../services/api-client";
+// When need types only during development not for production, 
+// add -D to the install command
+// npm install -D @types/ms
+import ms from "ms";
+
 
 const apiClient = new ApiClient<Genre>('/genres')
 
-export interface Genre {
+export interface Genre { 
   id: number
   name: string
   image_background: string
@@ -13,7 +18,7 @@ export interface Genre {
 const useGenres = () => useQuery({
   queryKey: ["genres"],
   queryFn: apiClient.getAll,
-  staleTime: 1000 * 60 * 60 * 24, // 24h
+  staleTime: ms('24h'),
   initialData: genres
 })
 
